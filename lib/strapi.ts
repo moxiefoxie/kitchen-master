bd9d4d9b5b872af36fea54288457abd905e5cd09
savvy <switham.gca@gmail.com>
2026-09-22 11:49:19 -0400

    Ignore stale development CMS URLs in production

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,11 +1,13 @@
 const DEFAULT_STRAPI_URL = 'https://devoted-angel-d525a2a640.strapiapp.com';
 const configuredStrapiUrl = process.env.STRAPI_URL ?? process.env.NEXT_PUBLIC_STRAPI_URL;
-const configuredForLocalhost = configuredStrapiUrl
-  ? /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?(\/|$)/i.test(configuredStrapiUrl)
+const configuredForTemporaryHost = configuredStrapiUrl
+  ? /^https?:\/\/((localhost|127\.0\.0\.1)(:\d+)?|[^/]+\.trycloudflare\.com)(\/|$)/i.test(
+      configuredStrapiUrl,
+    )
   : false;
 
 export const STRAPI_URL = (
-  process.env.NODE_ENV === 'production' && configuredForLocalhost
+  process.env.NODE_ENV === 'production' && configuredForTemporaryHost
     ? DEFAULT_STRAPI_URL
     : configuredStrapiUrl ?? DEFAULT_STRAPI_URL
 ).replace(/\/$/, '');
